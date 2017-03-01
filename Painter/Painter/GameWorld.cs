808de5f7389792d5aedfb563c076e7e3d8515e09
SoaringDylan <dylan@fampiera.nl>
2017-03-01 06:57:23 +0100

    Fixed funny result - End of Cannon Class
    onto Paint Can

--- Painter/Painter/GameWorld.cs
+++ Painter/Painter/GameWorld.cs
@@ -18,7 +18,8 @@ namespace Painter
             cannon = new ThreeColorGameObject("spr_cannon_red", "spr_cannon_blue", "spr_cannon_green");
             cannon.Position = new Vector2(58, 388);
             cannon_barrel = new RotatableSpriteGameObject("spr_cannon_barrel");
-            cannon_barrel.Position = new Vector2(40, 370);
+            cannon_barrel.Position = new Vector2(74, 404);
+            cannon_barrel.Origin = new Vector2(34, 34);
 
             this.Add(new SpriteGameObject("spr_background"));
 
