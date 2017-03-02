d39ace736399d7f7030d00a1000e392f3940dbeb
SoaringDylan <dylan@fampiera.nl>
2017-03-02 14:54:59 +0100

    Step J - Extra Stuff
    Moving Cannon sub-types into a Cannon class

---
+++ Painter/Painter/Cannon.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Painter
+{
+    class Cannon : GameObjectList
+    {
+        ThreeColorGameObject cannonColor;
+        RotatableSpriteGameObject cannon_barrel;
+
+        public Cannon()
+        {
+            cannonColor = new ThreeColorGameObject("spr_cannon_red", "spr_cannon_blue", "spr_cannon_green");
+            cannonColor.Position = new Vector2(52, 398);
+            cannon_barrel = new RotatableSpriteGameObject("spr_cannon_barrel");
+            cannon_barrel.Position = new Vector2(74, 404);
+            cannon_barrel.Origin = new Vector2(34, 34);
+        }
+
+        internal ThreeColorGameObject CannonColor
+        {
+            get
+            {
+                return cannonColor;
+            }
+
+            set
+            {
+                cannonColor = value;
+            }
+        }
+
+        internal RotatableSpriteGameObject Cannon_barrel
+        {
+            get
+            {
+                return cannon_barrel;
+            }
+
+            set
+            {
+                cannon_barrel = value;
+            }
+        }
+    }
+}
--- Painter/Painter/PainterGameWorld.cs
+++ Painter/Painter/PainterGameWorld.cs
@@ -10,8 +10,7 @@ namespace Painter
 {
     class PainterGameWorld : GameObjectList
     {
-        ThreeColorGameObject cannon;
-        RotatableSpriteGameObject cannon_barrel;
+        Cannon cannon;
         PaintCan can1, can2, can3;
         Ball ball;
         TextGameObject scoreObj;
@@ -26,12 +25,8 @@ namespace Painter
             scoreObj.Position = new Vector2(24,6);
             SpriteGameObject scoreBar = new SpriteGameObject("spr_scorebar");
             livesObj = new GameObjectList();
-            cannon = new ThreeColorGameObject("spr_cannon_red", "spr_cannon_blue", "spr_cannon_green");
-            cannon.Position = new Vector2(52, 398);
-            cannon_barrel = new RotatableSpriteGameObject("spr_cannon_barrel");
-            cannon_barrel.Position = new Vector2(74, 404);
-            cannon_barrel.Origin = new Vector2(34, 34);
-
+            cannon = new Cannon();
+            
             can1 = new PaintCan(Color.Red,450f);
             can2 = new PaintCan(Color.Green, 575f);
             can3 = new PaintCan(Color.Blue, 700f);
@@ -55,8 +50,8 @@ namespace Painter
             this.Add(can1);
             this.Add(can2);
             this.Add(can3);
-            this.Add(cannon_barrel);
-            this.Add(cannon);
+            this.Add(cannon.Cannon_barrel);
+            this.Add(cannon.CannonColor);
             this.Add(scoreObj);
             this.Add(livesObj);
 
@@ -104,19 +99,19 @@ namespace Painter
             base.HandleInput(inputHelper);
 
             if (inputHelper.KeyPressed(Keys.R))
-                cannon.Color = Color.Red;
+                cannon.CannonColor.Color = Color.Red;
             else if (inputHelper.KeyPressed(Keys.G))
-                cannon.Color = Color.Green;
+                cannon.CannonColor.Color = Color.Green;
             else if (inputHelper.KeyPressed(Keys.B))
-                cannon.Color = Color.Blue;
+                cannon.CannonColor.Color = Color.Blue;
 
-            double opposite = inputHelper.MousePosition.Y - cannon_barrel.GlobalPosition.Y;
-            double adjacent = inputHelper.MousePosition.X - cannon_barrel.GlobalPosition.X;
-            cannon_barrel.Angle = (float)Math.Atan2(opposite, adjacent);
+            double opposite = inputHelper.MousePosition.Y - cannon.Cannon_barrel.GlobalPosition.Y;
+            double adjacent = inputHelper.MousePosition.X - cannon.Cannon_barrel.GlobalPosition.X;
+            cannon.Cannon_barrel.Angle = (float)Math.Atan2(opposite, adjacent);
 
             if(inputHelper.MouseLeftButtonPressed() && !this.ball.Shooting)
             {
-                ball.Shoot(inputHelper, cannon, cannon_barrel);
+                ball.Shoot(inputHelper, cannon.CannonColor, cannon.Cannon_barrel);
             }
         }
 
@@ -157,8 +152,8 @@ namespace Painter
             this.can1.Reset();
             this.can2.Reset();
             this.can3.Reset();
-            this.cannon.Reset();
-            this.cannon_barrel.Reset();
+            this.cannon.CannonColor.Reset();
+            this.cannon.Cannon_barrel.Reset();
 
             base.Reset();
         }
